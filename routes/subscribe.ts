22cf1c70472ce3f3e211b76dcd4477236ee2e95d
Hildebrando de Castro Filho <dev@unitytech.dev.br>
2026-01-30 18:43:04 -0300

    Remove Fresh Configuration

--- routes/subscribe.ts
+++
@@ -1,8 +0,0 @@
-import { SubscriberController } from "../src/interfaces/http/SubscriberController.ts";
-
-export const handler = {
-    async POST(req: Request): Promise<Response> {
-        const controller = new SubscriberController();
-        return controller.subscribe(req);
-    }
-};
