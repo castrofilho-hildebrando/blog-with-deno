639032208a8566287b81fac9cb1fe39ac51d0a39
Hildebrando de Castro Filho <dev@unitytech.dev.br>
2026-01-30 19:11:10 -0300

    Fix serveDir headers format

--- main.ts
+++ main.ts
@@ -65,8 +65,8 @@ serve(async (req) => {
     return serveDir(req, {
         fsRoot: "public",
         urlRoot: "",
-        headers: [
-            ["Cache-Control", "public, max-age=3600"]
-        ]
+        headers: {
+            "Cache-Control": "public, max-age=3600"
+        }
     });
 });
